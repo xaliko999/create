08ef83f844e5cca7f0e8a8ae8428d52086650cbe
xaliko999 <xalikomasixullayev@gmail.com>
2024-09-09 19:30:00 +0500

    first elon

---
+++ app.js
@@ -0,0 +1,6 @@
+let a = prompt('Ismingizni kiriting')
+let b = prompt('Familiyangizni kiriting')
+let c = +prompt('Tugilgan yil, kun, oy ni kiriting')
+let j = prompt('Yoqtirgan faningizni kiriting')
+
+console.log(a,b,c,j);
